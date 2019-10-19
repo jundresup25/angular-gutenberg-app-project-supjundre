9dcdf41f0e9f22d3b5a126b78d5843a060552aef
jundresup25 <56750931+jundresup25@users.noreply.github.com>
2019-10-19 19:34:26 +0530

    Update book-list.component.ts

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,108 +1,98 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute  } from '@angular/router';
-import { BookService } from '../common/book.service';
-import { tap } from 'rxjs/operators';
+.section{
+    margin: 0 !important;
+}
 
-@Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
-})
-export class BookListComponent implements OnInit {
-  bookList: Array<any> = [];
-  jsonbookList: Array<any> = [];
-  bookType: string;
-  searchbook: string;
-  currentPage: number = 1;
-  scrollCallback;
-  clickIndex = 0;
-  constructor(private bookService:BookService, 
-              public router: Router,
-              public activateRoute: ActivatedRoute) {
-  }
+.bookSection{
+    background: #f5f5f5;
+    padding: 0;
+    // height: 100%;
+    height: 640px;
+    width: 500px;
+    overflow-y: scroll;
 
-  ngOnInit() {
-    this.bookType = this.activateRoute.snapshot.params['name'];
-    this.scrollCallback = this.getBooks.bind(this);
-    // this.getBookList();
-  }
-
-  getBooks() {
-    return this.bookService.getBooksByCategory(this.currentPage, this.bookType).pipe(tap(this.processData));
-  }
-
-  processData = (res) => {
-    this.currentPage++;
-    this.bookList = this.bookList.concat(res.results);
-  }
-  
-  // getBookList(){
-  //   this.bookService.getBookData().subscribe((res:any)=>{
-  //     this.jsonbookList = res.results;
-  //   })
-  // }
-
-  // getting book url if available html, pdf, text as per order
-  getBookURL(data){
-    let format = data.formats;
-    let obj = [format['text/html; charset=us-ascii'], format['text/html'], format['text/plain'], 
-               format['text/plain; charset=us-ascii'], format['application/pdf']];
-
-    let newObj = obj.filter((ele)=>{ return ele != undefined;})
-    let redirectBookLink = [];
-
-    for(let i=0; i<newObj.length; i++){
-      let extension = newObj[i].substr(newObj[i].lastIndexOf('.') + 1);  
-      if(extension == 'htm' || extension == 'html' || extension == 'noimages' || extension == 'images'){
-        redirectBookLink.push({"html": newObj[i]});
-      }
-      else if(extension == 'pdf'){
-        redirectBookLink.push({"pdf": newObj[i]});
-      }
-      else if(extension == 'utf-8' || extension == 'txt'){
-        redirectBookLink.push({"text": newObj[i]});
-      }
-      else{
-        console.log("no read version");
-      }
+    .book-list{    
+        padding-top: 13%;
     }
 
-    if(redirectBookLink.length > 0){
-      let value = Object.values(redirectBookLink[0]);
-      this.openBookInNewTab(value[0]);
+    .header{
+        background: #5454e9;
+        color: #fff;
+        height: 60px;
+        padding: 10px;
+        position: fixed;
+        width: 500px;
     }
-    else{
-      alert("No viewable version available");
+    
+    .title{
+        padding: 10px;
+        font-size: 16px;
+        text-transform: uppercase;
+        color: #286090;
+        margin-left: 10px;   
+        padding-bottom: 0 !important;
     }
-  }
 
-  // for opening book url in new tab
-  openBookInNewTab(url){
-    let a = document.createElement('a');
-    let linkText = document.createTextNode('fileLink');
-    a.appendChild(linkText);
-    a.setAttribute('target', '_blank');
-    a.href = url;
-    a.id = 'fileLink'+this.clickIndex;
-    let newID = 'fileLink'+this.clickIndex;
-    this.clickIndex++;
-    document.body.appendChild(a);
-    document.getElementById(newID).click();
-  }
+    .content{
+        display: inline-block;
+        padding: 10px 0px 10px 12px;
+        margin-left: 5px;
+    }
 
-  // for search filter
-  onSearchChange(val){
-    this.bookService.getBooksBySearchFilter(this.currentPage, this.bookType, val).subscribe((res:any)=>{
-      this.bookList = res.results;
-    })
-  }
+    .bookname{
+        font-size: 10px;
+        font-weight: 800;
+        padding: 10px 3px 0px 3px;
+        text-transform: uppercase;
+        // width: 145px;
+    }
 
-  // clear or cancel filter
-  cancelFilter(){
-    this.searchbook = "";
-    this.bookService.getBooksByCategory(this.currentPage, this.bookType).subscribe((res:any)=>{
-      this.bookList = res.results;
-    })
-  }
+    .authorname{
+        font-size: 10px;
+        padding: 2px 0px 0px 4px;
+        font-weight: 600;
+        color: #555;
+    }
 
+    .cancel{
+        font-size: 12px;
+        text-transform: uppercase;
+        padding-top: 10px;
+    }
+
+    .searchIcon{
+        position: relative;
+        left: -6px;
+        padding-bottom: 10px;
+        width: 35px;
+    }
+
+    .searchInput{
+        position: relative;
+        left: -34px;
+        background: transparent;
+        border: none;
+        border-bottom: 1px solid #fff;
+        width: 310px;
+        padding-bottom: 0px;
+        padding-left: 30px;
+
+        &:focus{
+            outline: none !important;
+        }
+    }
+
+    .closeIcon{
+        position: relative;
+        left: -35px;
+        width: 26px;
+        top: -40px;
+        padding-bottom: 10px;
+        bottom: 0px;
+        right: 0;
+        float: right;
+    }
+
+    div:focus{
+        outline: none;
+    }
 }
